6650f0216a36138c8ad4aa7b25285c1e2b2e57ce
Jayant Varma <jayant@cellcast.in>
2012-04-19 15:48:39 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vupoint::Application.config.secret_token = 'd16ee75af612b1bb998080aad4bb00426ddca1668c576e8d0ca258d69b5f72e032cbbeb6651c06bc8c74c6674273549dca187a4e6e761fd2ef15365d81f04c57'
