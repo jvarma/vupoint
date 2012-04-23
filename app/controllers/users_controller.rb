626b972ed06073004f3f34545e85bbb2bfef7351
Jayant Varma <jayant@cellcast.in>
2012-04-23 18:43:17 +0530

    pagination, index and sample data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+
+  before_filter :signed_in_user, only: [:edit, :update, :index]
+  before_filter :correct_user, only: [:edit, :update]
+
+  def index
+    @users = User.paginate(page: params[:page])
+    #@title = "#{@users.count} users"
+  end
+
   def new
   	@title = "sign up"
   	@user = User.new
@@ -19,4 +28,50 @@ class UsersController < ApplicationController
   		render :new
   	end
   end
+
+  def edit
+    @title = @user.name.downcase
+  end
+
+  def update
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated successfully!"
+      sign_in @user
+      redirect_to @user
+    else
+      render :edit
+    end
+
+  end
+
+  private
+    
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_path, notice: "Please sign in first!"
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+
+
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
