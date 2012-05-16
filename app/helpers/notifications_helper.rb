07ff000c15aa11fc733978b796305b89b36c945e
Jayant Varma <jayant@cellcast.in>
2012-05-17 02:10:59 +0530

    x

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -1,15 +1,9 @@
 module NotificationsHelper
 	def get_notification_content_partial_name(notification)
-		if !notification.classname.nil?
-			"#{notification.classname.downcase}_content"
-		else
-			"notification.classname is nil"
-		end
-		#"get_notification_content_partial_name"
+		"#{notification.classname.downcase}_content"
 	end
 
 	def get_notification_actions_partial_name(notification)
-		#"#{notification.classname.downcase}_actions"
-		"get_notification_actions_partial_name"
+		"#{notification.classname.downcase}_actions"
 	end
 end
