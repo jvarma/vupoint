2b593dba05d868991739bd6050d7b8291e1f49b3
Jayant Varma <jayant@cellcast.in>
2012-05-17 10:52:46 +0530

    x

--- app/helpers/notifications_helper.rb
+++
@@ -1,9 +0,0 @@
-module NotificationsHelper
-	def get_notification_content_partial_name(notification)
-		"#{notification.classname.downcase}_content"
-	end
-
-	def get_notification_actions_partial_name(notification)
-		"#{notification.classname.downcase}_actions"
-	end
-end
