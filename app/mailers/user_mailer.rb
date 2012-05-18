2abbe9d7537fe5f99586e8942a3c04ea421b4ef6
Jayant Varma <jayant@cellcast.in>
2012-05-18 14:37:54 +0530

    some changes to the mailer views

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,23 +2,11 @@ class UserMailer < ActionMailer::Base
   default from: "izebrg@gmail.com"
 
   def confirm_email(user)
-  	if Rails.env.development?
-      @host = "localhost:3000"
-    else
-      @host = "vupnt.herokuapp.com"
-    end
-    @user = user
+  	@user = user
   	mail(to: user.email, subject: "Your view counts! Please confirm your email id.")
   end
 
   def invitation_for_debate(debate_invite)
-    if Rails.env.development?
-      @host = "localhost:3000"
-    else
-      @host = "vupnt.herokuapp.com"
-    end
-
-    #@debate_invite = DebateInvite.find(debate_invite)
     @sender = User.find(debate_invite.sender_id)
     @debate = Debate.find(debate_invite.debate_id)
     @receiver = User.find_by_id(debate_invite.receiver_id)
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,5 +35,5 @@ Vupoint::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
-  config.action_mailer.default_url_options = { :host => "http://localhost:3000/" }
+  config.action_mailer.default_url_options = { :host => "localhost:3000" }
 end
